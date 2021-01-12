593b5fbcdbad32b9bd954ed09c92791859a39c95
osaelh <oussamaelh@gmail.com>
2021-01-12 23:21:40 +0100

    client manager

---
+++ clientsManager/clients_manager/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace clients_manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(674, 655);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(140, 39);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(846, 655);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(140, 39);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(1013, 655);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(140, 39);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(1178, 655);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(140, 39);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(659, 699);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(723, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Full name:";
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(881, 40);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(300, 24);
+            this.comboBox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(723, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Adress:";
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(881, 183);
+            this.txtAdress.Multiline = true;
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(300, 147);
+            this.txtAdress.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(723, 399);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "City:";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(881, 399);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(300, 22);
+            this.txtCity.TabIndex = 10;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(881, 102);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(300, 22);
+            this.txtId.TabIndex = 11;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(1196, 40);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(140, 39);
+            this.btnShow.TabIndex = 12;
+            this.btnShow.Text = "Show info";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 715);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ clientsManager/clients_manager/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace clients_manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cnx = new SqlConnection("Data Source=ANDROID-87C5AZ0; Initial Catalog=clientDb; Integrated Security =true");
+        DataSet DS = new DataSet();
+        SqlDataAdapter DA;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DA = new SqlDataAdapter("Select * from clients",cnx);
+            DA.Fill(DS,"clt");
+            dataGridView1.DataSource = DS.Tables["clt"];
+            for (int i = 0; i < DS.Tables["clt"].Rows.Count; i++)
+            {
+                comboBox.Items.Add(DS.Tables["clt"].Rows[i][1]);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            comboBox.Items.Add(comboBox.Text);
+            DataRow row = DS.Tables["clt"].NewRow();
+
+            /* DataColumn dataColumn = DS.Tables["clt"].Columns.Add("id", typeof(Int32));
+             dataColumn.AutoIncrement = true;
+             dataColumn.AutoIncrementStep = 1;
+             dataColumn.AutoIncrementSeed = 200;*/
+
+            row[0] = (int.Parse(DS.Tables["clt"].Rows[DS.Tables["clt"].Rows.Count - 1][0].ToString()) + 1).ToString();
+            row[1] = comboBox.Text;
+            row[2] = txtAdress.Text;
+            row[3] = txtCity.Text;
+            DS.Tables["clt"].Rows.Add(row);
+            cleaner();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow row in DS.Tables["clt"].Rows)
+            {
+                if (txtId.Text == row[0].ToString())
+                {
+                    row.Delete();
+                }
+
+                //int p = -1;
+
+
+                //for (int i = 0; i < DS.Tables["clt"].Rows.Count; i++)
+                //{
+                //    if (txtId.Text== DS.Tables["clt"].Rows[i][0].ToString())
+                //    {
+                //        p = i;
+                //    }
+                //}
+                //if (p== -1)
+                //{
+                //    MessageBox.Show("client not found");
+                //}
+                //else
+                //{
+                //    DS.Tables["clt"].Rows[p].Delete();
+                //    MessageBox.Show("client deleted sucsesfully");
+                //}
+            }
+            }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            int p = -1;
+            for (int i = 0; i < DS.Tables["clt"].Rows.Count; i++)
+            {
+                if (txtId.Text == DS.Tables["clt"].Rows[i][0].ToString())
+                {
+                    p = i;
+                }
+            }
+            if (p == -1)
+            {
+                MessageBox.Show("client not found");
+            }
+            else
+            {
+                DS.Tables["clt"].Rows[p][0]=int.Parse(txtId.Text);
+                DS.Tables["clt"].Rows[p][1] = comboBox.Text;
+                DS.Tables["clt"].Rows[p][2] = txtAdress.Text;
+                DS.Tables["clt"].Rows[p][3] = txtCity.Text;
+                MessageBox.Show("client modified sucsesfully");
+            }
+            cleaner();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommandBuilder builder = new SqlCommandBuilder(DA);
+                DA.Update(DS, "clt");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void cleaner()
+        {
+            comboBox.Text = null;
+            txtId.Text = null;
+            txtAdress.Text = null;
+            txtCity.Text = null;
+        }
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            int p = -1;
+            for (int i = 0; i < DS.Tables["clt"].Rows.Count; i++)
+            {
+                if (comboBox.Text == DS.Tables["clt"].Rows[i][1].ToString())
+                {
+                    p = i;
+                }
+            }
+            if (p == -1)
+            {
+                MessageBox.Show("client not found");
+            }
+            else
+            {
+              
+                txtId.Text = Convert.ToString(DS.Tables["clt"].Rows[p][0]);
+                txtAdress.Text = Convert.ToString(DS.Tables["clt"].Rows[p][2]);
+                txtCity.Text = Convert.ToString(DS.Tables["clt"].Rows[p][3]);
+
+            }
+        }
+    }
+}
